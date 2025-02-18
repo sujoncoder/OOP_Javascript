af7c5c6d16773bc4ebd191eca7c1a39e6f83bcba
sujoncoder <sujonsheikh.in@gmail.com>
2025-02-18 03:49:16 -0800

    start with simple obj

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // LEARNING OOP IN JAVASCRIPT
+const person = {
+    name: "sujon",
+    email: "sujon@gmail.com"
+};
+person.isMarried = false;
+person.contact = "01999-986919";
+person.hello = () => console.log("Hello Sujon")
+console.log(person.hello())
